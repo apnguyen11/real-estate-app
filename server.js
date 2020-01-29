580e19acfe2b709fba5b0e5173469c1a3800966b
Andy Nguyen <apnguyen11@yahoo.com>
2020-01-29 17:35:29 -0600

    Facebook authentication

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const app = express();
+// const mongoose = require('mongoose')
 
+// mongoose.connect('mongodb+srv://apnguyen11:' + process.env.MONGO_ATLAS_PW + '@real-estate-db-n3a77.mongodb.net/test?retryWrites=true&w=majority', {
+//     useMongoClient: true
+// })
 //Real data will come from database, initial hardcoded data
+app.use('/posts', () => {
+    console.log('This is middleware running')
+})
+
 app.get('/api/businesses', (req, res) => {
     const businesses = [
         {id:1, name: 'Starbucks', location: '123 Main st'},
@@ -12,6 +20,15 @@ app.get('/api/businesses', (req, res) => {
 
     res.json(businesses)
 })
+
+app.get('/', (req, res) => {
+    res.send('WE are on home page')
+})
+
+app.get('/posts', (req, res) => {
+    res.send('we are on posts')
+})
+
 
 const port = 5000;
 
